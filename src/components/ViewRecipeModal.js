50a729bee5b9376f1df55db7cfd01343125a5163
Teodora Smaranda <teodora.smaranda@emea.nttdata.com>
2023-06-20 21:27:44 +0300

    -

--- src/components/ViewRecipeModal.js
+++ src/components/ViewRecipeModal.js
@@ -1,3 +1,5 @@
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -17,11 +19,30 @@ const ViewRecipeModal = ({ recipe, isOpen, onHandleClose }) => {
       }}
       fullScreen
     >
-      <DialogTitle>Add Recipe</DialogTitle>
+      <DialogTitle>
+        <b>{recipe.recipe_title}</b>
+      </DialogTitle>
       <DialogContent>
-      <Typography variant="body2" color="text.secondary">
-            {recipe.preparation}
-          </Typography>
+        <CardMedia
+          sx={{ maxWidth: 345 }}
+          image={recipe.img}
+          component="img"
+        ></CardMedia>
+        <CardContent>
+          <Typography>Preparation time: {recipe.preparation_time}</Typography>
+          <hr />
+          <Typography>Baking time: {recipe.baking_time}</Typography>
+        </CardContent>
+
+        <Typography variant="" color="text.secondary">
+          Ingredients:
+          {recipe.ingredients}
+        </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+          Preparation:
+          {recipe.preparation}
+        </Typography>
       </DialogContent>
     </Dialog>
   );
